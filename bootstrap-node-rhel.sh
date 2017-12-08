10bb2d2553797abd72e28f5177ffa9f2ef011264
David R. Fischer <fischer.d.r@gmal.com>
2017-12-07 19:52:43 -0700

    re-added vagrant submodules

--- bootstrap-node-rhel.sh
+++ bootstrap-node-rhel.sh
@@ -6,29 +6,29 @@
 # Downgrade Puppet on box from 4.x to 3.x for Foreman 1.9 
 # http://theforeman.org/manuals/1.9/index.html#3.1.2PuppetCompatibility
 
-# Update system first
-sudo yum install -y deltarpm
-sudo yum update -y
-
-echo "Updating or Replacing current Puppet"
-
-sudo rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm && \
-    sudo yum -y erase puppet-agent && \
-    sudo rm -f /etc/yum.repos.d/puppetlabs-pc1.repo && \
-    sudo yum clean all && \
-    sudo yum -y install puppet nano nmap-ncat
+	# Update system first
+	sudo yum install -y deltarpm
+	sudo yum update -y
+	
+	echo "Updating or Replacing current Puppet"
+	
+	sudo rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm && \
+	sudo yum -y erase puppet && \
+	sudo rm -f /etc/yum.repos.d/puppetlabs.repo && \
+	sudo yum clean all &&\
+	sudo yum -y install puppet-agent nano nmap-ncat
 
     # Add agent section to /etc/puppet/puppet.conf
     # Easier to set run interval to 120s for testing (reset to 30m for normal use)
     # https://docs.puppetlabs.com/puppet/3.8/reference/config_about_settings.html
-    echo "" | sudo tee --append /etc/puppet/puppet.conf 2> /dev/null && \
-    echo "    server = theforeman.example.com" | sudo tee --append /etc/puppet/puppet.conf 2> /dev/null && \
-    echo "    runinterval = 120s" | sudo tee --append /etc/puppet/puppet.conf 2> /dev/null
+    echo "" | sudo tee --append  /etc/puppetlabs/puppet/puppet.conf 2> /dev/null && \
+    echo "    server = theforeman.example.com" | sudo tee --append /etc/puppetlabs/puppet/puppet.conf 2> /dev/null && \
+    echo "    runinterval = 120s" | sudo tee --append /etc/puppetlabs/puppet/puppet.conf 2> /dev/null
 
-    sudo service puppet stop
+    sudo service puppet-agent stop
     #sudo service puppet start
-    sudo puppet resource service puppet ensure=running enable=true
-    sudo puppet agent --enable
+    sudo /opt/puppetlabs/puppet/bin/puppet resource service puppet-agent ensure=running enable=true
+    sudo /opt/puppetlabs/puppet/bin/puppet agent --enable
 
     # Unless you have Foreman autosign certs, each agent will hang on this step until you manually
     # sign each cert in the Foreman UI (Infrastrucutre -> Smart Proxies -> Certificates -> Sign)
